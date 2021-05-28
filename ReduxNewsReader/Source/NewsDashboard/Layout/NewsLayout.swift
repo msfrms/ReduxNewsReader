70cfba37e00ca3e09af33303ab05ae6c75822976
Radaev Mikhail <msfrms@gmail.com>
2021-05-28 13:27:00 +0300

    добавил верстку ячейки новости

---
+++ ReduxNewsReader/Source/NewsDashboard/Layout/NewsLayout.swift
@@ -0,0 +1,90 @@
+//
+//  NewsLayout.swift
+//  ReduxNewsReader
+//
+//  Created by Mikhail Radaev on 28.05.2021.
+//
+
+import LayoutKit
+import Kingfisher
+
+public final class NewsLayout: StackLayout<UIView> {
+    
+    public struct Props {
+        public let title: String
+        public let subtitle: String
+        public let coverUrl: URL?
+        public let onTap: Command
+    }
+    
+    public init(props: Props, viewReuseId: String) {
+        
+        let texts = StackLayout(
+            axis: .vertical,
+            spacing: 5,
+            distribution: .leading,
+            sublayouts: [
+                LabelLayout(
+                    attributedText: .init(
+                        string: props.title.uppercased(),
+                        attributes: [
+                            .font: UIFont.systemFont(ofSize: 14, weight: .heavy),
+                            .foregroundColor: Asset.Colors.blue.color
+                        ]
+                    ),
+                    viewReuseId: "\(viewReuseId).title"
+                ),
+                LabelLayout(
+                    attributedText: .init(
+                        string: props.subtitle,
+                        attributes: [
+                            .font: UIFont.systemFont(ofSize: 14, weight: .medium),
+                            .foregroundColor: Asset.Colors.black.color,
+                            .paragraphStyle: {
+                                let paragraph = NSMutableParagraphStyle()
+                                paragraph.alignment = .left
+                                paragraph.lineBreakMode = .byWordWrapping
+                                return paragraph
+                            }()
+                        ]
+                    ),
+                    numberOfLines: 2,
+                    viewReuseId: "\(viewReuseId).subtitle"
+                )
+            ]
+        )
+        
+        super.init(
+            axis: .horizontal,
+            spacing: 23,
+            distribution: .leading,
+            viewReuseId: viewReuseId,
+            sublayouts: [
+                SizeLayout<UIImageView>(
+                    width: 92,
+                    height: 92,
+                    viewReuseId: "\(viewReuseId).icon",
+                    config: { view in
+                        view.kf.setImage(with: props.coverUrl)
+                        view.layer.cornerRadius = 16
+                        view.clipsToBounds = true
+                        view.backgroundColor = Asset.Colors.darkGray.color
+                    }
+                ),
+                StackLayout(
+                    axis: .vertical,
+                    distribution: .fillEqualSpacing,
+                    sublayouts: [
+                        SizeLayout(height: 1),
+                        texts,
+                        SizeLayout(height: 1)
+                    ]
+                )
+            ],
+            config: { view in
+                view.layer.cornerRadius = 16
+                view.backgroundColor = .white
+            }
+        )
+    }
+}
