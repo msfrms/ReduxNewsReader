755c23eabc67f423699a708215539614ad49f9dd
Radaev Mikhail <msfrms@gmail.com>
2021-06-02 13:11:19 +0300

    add reducers

--- ReduxNewsReader/Source/Redux/AppState.swift
+++ ReduxNewsReader/Source/Redux/AppState.swift
@@ -13,6 +13,16 @@ public struct AppState {
     public let newsCard: NewsCardState
     public let newsListAll: NewsListAllState
     public let newsCardAll: NewsListCardAllState
+    
+    public func reduce(action: Action) -> AppState {
+        AppState(
+            newsList: newsList.reduce(action: action),
+            newsLatest: newsLatest.reduce(action: action),
+            newsCard: newsCard.reduce(action: action),
+            newsListAll: newsListAll.reduce(action: action),
+            newsCardAll: newsCardAll.reduce(action: action)
+        )
+    }
 }
 
 extension AppState {
