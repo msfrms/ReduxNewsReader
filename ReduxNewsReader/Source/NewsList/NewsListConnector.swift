5e3adaf9d5ea6f4fb99be2996187157d2d1cc25a
Radaev Mikhail <msfrms@gmail.com>
2021-06-03 19:04:18 +0300

    fix

---
+++ ReduxNewsReader/Source/NewsList/NewsListConnector.swift
@@ -0,0 +1,79 @@
+//
+//  NewsListConnector.swift
+//  ReduxNewsReader
+//
+//  Created by Mikhail Radaev on 03.06.2021.
+//
+
+import Foundation
+
+public enum NewsListConnector {
+    
+    public static func connect(
+        store: Store<AppState>,
+        to view: NewsListViewController,
+        category: NewsCategory) {
+        view.didLoad = Command {
+            store.dispatch(action: NewsListAction.newsListByCategory(category, .start(0)))
+        }
+        let unsubscribe = store.subscribe(command: CommandWith<AppState> { [weak view] state in
+            guard let view = view else {
+                return
+            }
+            guard let newsState = state.newsList.byCategory[category] else {
+                view.render(props: .inProgress)
+                return
+            }
+            
+            switch newsState.status {
+            case .inProgress, .none:
+                view.render(props: .inProgress)
+            case .failed:
+                view.render(
+                    props: .empty(
+                        .init(
+                            title: "Упс, что то пошло не так",
+                            description: "Возможно у вас нет интернета",
+                            onRetry: .nop
+                        )
+                    )
+                )
+            case .success:
+                let newsProps: [NewsLayout.Props] = newsState.ids
+                    .compactMap { state.newsListAll.byId[$0] }
+                    .map { news in
+                        .init(
+                            title: news.title,
+                            subtitle: news.description,
+                            coverUrl: news.coverUrl,
+                            onTap: .nop
+                        )
+                    }
+                view.render(
+                    props: .list(
+                        .init(
+                            items: newsProps,
+                            onFirstPage: Command {
+                                store.dispatch(
+                                    action: NewsListAction.newsListByCategory(category, .start(0))
+                                )
+                            },
+                            onNextPage: Command {
+                                store.dispatch(
+                                    action: NewsListAction.newsListByCategory(
+                                        category,
+                                        .start(newsState.page + 1)
+                                    )
+                                )
+                            }
+                        )
+                    )
+                )
+            }
+            
+            
+        }.observe(queue: .main))
+        
+        view.deinitCommand = unsubscribe
+    }
+}
