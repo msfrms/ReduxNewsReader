28bae1432f9025a3ef5c71b1a7679e113fefd2bd
Radaev Mikhail <msfrms@gmail.com>
2021-06-03 07:40:41 +0300

    add status for latest news state

--- ReduxNewsReader/Source/Redux/NewsLatestState.swift
+++ ReduxNewsReader/Source/Redux/NewsLatestState.swift
@@ -9,13 +9,20 @@ import Foundation
 
 public struct NewsLatestState {
     public let ids: [NewsList.Id]
+    public let status: Status
     
     public func reduce(action: Action) -> NewsLatestState {
         switch action {
         
+        case NewsListAction.newsListByCategory(.history, .start):
+            return .init(ids: [], status: .inProgress)
+            
+        case NewsListAction.newsListByCategory(.history, .failed):
+            return .init(ids: [], status: .failed)
+        
         case NewsListAction.newsListByCategory(.history, .loaded(let news, _)):
             let newsIds = news[..<4].map { $0.id }
-            return .init(ids: newsIds)
+            return .init(ids: newsIds, status: .success)
             
         default:
             return self
@@ -25,6 +32,6 @@ public struct NewsLatestState {
 
 extension NewsLatestState {
     static var initial: NewsLatestState {
-        .init(ids: [])
+        .init(ids: [], status: .none)
     }
 }
